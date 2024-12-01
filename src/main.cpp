aeab8f3dd25b91057b2e778b0b368d30473b6823
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2024-12-02 00:28:25 +0300

    add func for running all algo from stdin with any number of strings to predict

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 
 #include <iostream>
 #include <sstream>
+
 #include "grammar/grammar.hpp"
 #include "cyk.hpp"
+
+void runAlgo ();
+
 int main () {
     // NeTerminal a('a');
     // NeTerminal f('a');
@@ -23,21 +27,47 @@ int main () {
     //     std::cout << "after reading [" << c1 << "], gcount() == " <<  s1.gcount() << '\n';
     // }
 
+//     Grammar grammar;
+//     grammar.CreateFromStdin();
+//     grammar.Print();
+
+//     CYK cyk;
+//     cyk.fit(grammar);
+//     bool res = cyk.predict("ab");
+//     std::cout << "res for ab = " << res << "\n";
+//     res = cyk.predict("abb");
+//     std::cout << "res for abb = " << res << "\n";
+
+//     res = cyk.predict("aab");
+//     std::cout << "res for aab = " << res << "\n";
+//     res = cyk.predict("aaaabbbb");
+//     std::cout << "res for aaaabbbb = " << res << "\n";
+//     res = cyk.predict("bab");
+//     std::cout << "res for bab = " << res << "\n";
+    runAlgo ();
+}
+
+void runAlgo () {
     Grammar grammar;
     grammar.CreateFromStdin();
-    grammar.Print();
+    // grammar.Print();
 
     CYK cyk;
     cyk.fit(grammar);
-    bool res = cyk.predict("ab");
-    std::cout << "res for ab = " << res << "\n";
-    res = cyk.predict("abb");
-    std::cout << "res for abb = " << res << "\n";
 
-    res = cyk.predict("aab");
-    std::cout << "res for aab = " << res << "\n";
-    res = cyk.predict("aaaabbbb");
-    std::cout << "res for aaaabbbb = " << res << "\n";
-    res = cyk.predict("bab");
-    std::cout << "res for bab = " << res << "\n";
+    size_t N; 
+    std::cin >> N;
+
+    std::vector<std::string> stringsToPredict;
+    for (size_t i = 0; i < N; ++i) {
+        std::string str;
+        std::cin >> str;
+        stringsToPredict.push_back(str);
+    }
+
+    bool res;
+    for (size_t i = 0; i < N; ++i) {
+        res = cyk.predict(stringsToPredict[i]);
+        std::cout << "res for " << stringsToPredict[i] << " = " << res << "\n";
+    }
 }
