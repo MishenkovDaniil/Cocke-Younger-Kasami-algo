3a11dafd608bf3ab93a61cc314b3ce7ed18078b9
Mishenkov Daniil <mishenkovdaniil@mail.ru>
2024-12-07 11:08:41 +0300

    add comments and uncommented CYK after debugging

--- src/main.cpp
+++ src/main.cpp
@@ -8,49 +8,16 @@
 void runAlgo ();
 
 int main () {
-    // NeTerminal a('a');
-    // NeTerminal f('a');
-
-    // std::cout << (a == f);
-
-
-    
-    // std::istringstream s1("Hello, world.");
-    // for (int i = 1; i < 33; ++i) {
-    //     char c1 = s1.get(); // reads 'H'
-    //     std::cout << "after reading [" << int(c1) << "], gcount() == " <<  s1.gcount() << '\n';
-    // }
-    // std::istringstream s1("Hello, world.\n");
-    // char c1 = 0;
-    // for (int i = 1; i < 33; ++i) {
-    //     s1 >> c1;
-    //     std::cout << "after reading [" << c1 << "], gcount() == " <<  s1.gcount() << '\n';
-    // }
-
-//     Grammar grammar;
-//     grammar.CreateFromStdin();
-//     grammar.Print();
-
-//     CYK cyk;
-//     cyk.fit(grammar);
-//     bool res = cyk.predict("ab");
-//     std::cout << "res for ab = " << res << "\n";
-//     res = cyk.predict("abb");
-//     std::cout << "res for abb = " << res << "\n";
-
-//     res = cyk.predict("aab");
-//     std::cout << "res for aab = " << res << "\n";
-//     res = cyk.predict("aaaabbbb");
-//     std::cout << "res for aaaabbbb = " << res << "\n";
-//     res = cyk.predict("bab");
-//     std::cout << "res for bab = " << res << "\n";
     runAlgo ();
 }
 
 void runAlgo () {
     Grammar grammar;
     grammar.CreateFromStdin();
-    grammar.Print();
+    /// grammar.Print();
+
+    grammar.ConvertToChomsky();
+    /// grammar.Print();
 
     CYK cyk;
     cyk.fit(grammar);
@@ -88,4 +55,12 @@ abb
 aaaabbbb
 bab
 bba
+*/
+/*
+results
+ab - 1
+abb - 1
+aaaabbbb - 1
+bab - 0
+bba - 0
 */
